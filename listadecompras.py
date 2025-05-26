9b9c354577c6770ad45bdb8630b52f5c4e11748a
Ana Luiza <analuizastabile04@gmail.com>
2025-05-26 15:26:44 -0400

    Add files via upload

--- listadecompras.py
+++ listadecompras.py
@@ -1,17 +1,40 @@
-compras=[]
-resposta=""
+# # # # # compras=[]
+# # # # # resposta=""
+# # # # # while resposta.upper()!= "SAIR":
+# # # # #     resposta=input("deseja incluir algum item na lista? Caso queira finalizar, 'SAIR' ")
+# # # # #     if resposta=="sim":
+# # # # #         item_extra=input("adicione item: ")
+# # # # #         compras.append(item_extra)
+# # # # #         print(compras)
+# # # # #     elif resposta=="não":
+# # # # #         item_fora=input("Informe o item  ser removido: ")
+# # # # #         compras.remove(item_fora)
+# # # # #         print(compras)
+# # # # #     else:
+# # # # #         print("lista finalizada, essa é sua lista")
+# # # # #         print(compras)
 
 
-while resposta.upper()!= "SAIR":
-    resposta=input("deseja incluir algum item na lista? Caso queira finalizar, 'SAIR' ")
-    if resposta=="sim":
-        item_extra=input("adicione item: ")
-        compras.append(item_extra)
-        print(compras)
-    elif resposta=="não":
-        item_fora=input("Informe o item  ser removido: ")
-        compras.remove(item_fora)
-        print(compras)
-    else:
-        print("lista finalizada, essa é sua lista")
-        print(compras)
+# # # # listas[]
+# # # # dicionario{chave:valor}
+# # # # tupla()
+# # # # conjunto= set()
+
+
+# # # teste=[1,2,3,5]
+# # # print(teste)
+# # # teste.append(4)
+# # # print(teste)
+
+# # teste=(1,2,3,5)
+# # print(teste)
+# # teste.append(4)
+# # print(teste)
+# # #TUPLA É IUMTÁVEL, NÃO DÁ PRA ADICIONAR OU REMOVER ITENS DE DENTRO DA TUPLA
+
+# conjunto
+# teste={1,2,3,5,5,6,6}
+# print(teste)
+# teste.remove(1)
+# print(teste)
+# teste.append(1)
